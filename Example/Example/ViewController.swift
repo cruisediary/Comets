f93e49b3a183578d5db5a224af5a2facaec93fe3
Cruz <cruzdiary@gmail.com>
2018-10-18 00:52:01 +0900

    Add empty example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Cruz on 18/10/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
