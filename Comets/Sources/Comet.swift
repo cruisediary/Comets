6090b0c70b576f892aa0ef9ec74451e6a7697a37
Cruz <cruzdiary@gmail.com>
2018-10-21 14:40:27 +0900

    Fix Comet using gradient layer without resources assets

--- Comets/Sources/Comet.swift
+++ Comets/Sources/Comet.swift
@@ -3,6 +3,12 @@ public struct Comet {
     public var endPoint: CGPoint
     public var lineColor: UIColor
     
+    public init(startPoint: CGPoint, endPoint: CGPoint, lineColor: UIColor) {
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+        self.lineColor = lineColor
+    }
+    
     public var linePath: UIBezierPath {
         let path = UIBezierPath()
         path.move(to: startPoint)
@@ -14,7 +20,7 @@ public struct Comet {
         // create the line as a CAShapeLayer
         let lineLayer = CAShapeLayer()
         lineLayer.path = linePath.cgPath
-        lineLayer.lineWidth = 4
+        lineLayer.lineWidth = 0.5
         lineLayer.strokeColor = lineColor.cgColor
         return lineLayer
     }
@@ -27,9 +33,7 @@ public struct Comet {
         
         // create comet cell
         let cell = CAEmitterCell()
-        cell.contents = UIImage(named: "Comet")?
-            .rotate(radians: calculateAngle())?
-            .cgImage
+        cell.contents = contents
         cell.birthRate = 0.2 * Float(Int.random(in: 500 ... 2000 )) / 1000
         cell.lifetime = 10.0
         cell.velocity = 600
@@ -39,6 +43,24 @@ public struct Comet {
         // add cell to the emitter
         emitter.emitterCells = [cell]
         return emitter
+    }
+    
+    public var contents: Any? {
+        let cometLayer = CAGradientLayer()
+        cometLayer.colors = [UIColor.white.withAlphaComponent(0.0).cgColor, UIColor.white.cgColor]
+        cometLayer.cornerRadius = 0.25
+        cometLayer.frame = CGRect(x: 0, y: 0, width: 80, height: 0.5)
+        cometLayer.locations = [0.0, 1.0]
+        cometLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        cometLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        
+        UIGraphicsBeginImageContextWithOptions(cometLayer.bounds.size, cometLayer.isOpaque, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        cometLayer.render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()?
+            .rotate(radians: calculateAngle())?
+            .cgImage
     }
     
     public func calculateAngle() -> CGFloat {
