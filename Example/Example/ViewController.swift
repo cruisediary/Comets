179dcda2cfad4f422c457741ebdbb9104c3bf58e
Cruz <cruzdiary@gmail.com>
2018-10-22 00:19:20 +0900

    Add comet example :)

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Comets
-import Flags
 import Gradients
 import SnapKit
 
@@ -17,7 +16,7 @@ class ViewController: UIViewController {
     let backgroundLayer = Gradients.amourAmour.layer
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = Flag(countryCode: "SE")?.emoji
+        label.text = "☄️Comets"
         label.font = UIFont.systemFont(ofSize: 36)
         label.textColor = .white
         return label
@@ -28,9 +27,42 @@ class ViewController: UIViewController {
         
         view.layer.addSublayer(backgroundLayer)
         view.addSubview(titleLabel)
+        
         titleLabel.snp.makeConstraints { maker in
             maker.center.equalToSuperview()
         }
+        
+        // Customize your comet
+        let width = view.bounds.width
+        let height = view.bounds.height
+        let comets = [Comet(startPoint: CGPoint(x: 100, y: 0),
+                            endPoint: CGPoint(x: 0, y: 100),
+                            lineColor: UIColor.white.withAlphaComponent(0.2)),
+                      Comet(startPoint: CGPoint(x: 0.4 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.8 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2)),
+                      Comet(startPoint: CGPoint(x: 0.8 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.2 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2)),
+                      Comet(startPoint: CGPoint(x: width, y: 0.2 * height),
+                            endPoint: CGPoint(x: 0, y: 0.25 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2)),
+                      Comet(startPoint: CGPoint(x: 0, y: height - 0.8 * width),
+                            endPoint: CGPoint(x: 0.6 * width, y: height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2)),
+                      Comet(startPoint: CGPoint(x: width - 100, y: height),
+                            endPoint: CGPoint(x: width, y: height - 100),
+                            lineColor: UIColor.white.withAlphaComponent(0.2)),
+                      Comet(startPoint: CGPoint(x: 0, y: 0.8 * height),
+                            endPoint: CGPoint(x: width, y: 0.75 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2))]
+        
+        // draw track and animate
+        for comet in comets {
+            view.layer.addSublayer(comet.drawLine())
+            view.layer.addSublayer(comet.animate())
+        }
+    
     }
 
     override func viewDidLayoutSubviews() {
