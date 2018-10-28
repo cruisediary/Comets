ace7b8acd7e9bddd779eaf469eebb5e894f166d0
Cruz <cruzdiary@gmail.com>
2018-10-28 16:23:41 +0900

    Support Carthage

---
+++ Comets/Comets.h
@@ -0,0 +1,19 @@
+//
+//  Comets.h
+//  Comets
+//
+//  Created by Cruz on 28/10/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Comets.
+FOUNDATION_EXPORT double CometsVersionNumber;
+
+//! Project version string for Comets.
+FOUNDATION_EXPORT const unsigned char CometsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Comets/PublicHeader.h>
+
+
